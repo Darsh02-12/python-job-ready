7476c88dc4b61a05cc44430c418d414f80605784
Darsh jatt <jattdarsh51@gmail.com>
2026-09-14 18:46:12 +0530

    Added the excetion handling in stack: peek() and pop()

--- week2/stack.py
+++ week2/stack.py
@@ -1,3 +1,6 @@
+class StackEmptyError(Exception):
+    pass
+
 class Stack:
     def __init__(self):
         self.items=[]
@@ -6,10 +9,26 @@ class Stack:
         self.items.append(items)
 
     def pop(self):
+        if self.is_empty():
+            raise StackEmptyError("The stack is empty to pop")
         return self.items.pop()
 
     def peek(self):
+        if self.is_empty:
+            raise StackEmptyError("The stack is Empty")
         return self.items[-1]
 
     def is_empty(self):
         return len(self.items) == 0
+
+s=Stack()
+
+try:
+    print(s.peek())
+except StackEmptyError as e:
+    print(f"Empty stack: {e}")
+
+try:
+    print(s.pop())
+except StackEmptyError as e:
+    print(f"Empty stack: {e}")
